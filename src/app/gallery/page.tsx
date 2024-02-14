d4aebcdcf2712154b09bde179ba12e393cd56e93
Samuel Rodriguez <samuelyret@gmail.com>
2024-02-14 12:00:38 +0100

    Add info about the gallery.

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,11 @@
+export default function Gallery() {
+  return (
+    <>
+      <p>
+        Important: The current pictures do not show the shutters, mosquito nets
+        and curtains which have been recently installed. Please note that all
+        rooms have shutters and curtains for privacy and light blocking.
+      </p>
+    </>
+  );
+}
