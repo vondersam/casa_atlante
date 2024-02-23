6774274a86aedfc04183c0644dc460f5ee7e4487
Samuel Rodriguez <samuelyret@gmail.com>
2024-02-23 18:57:54 +0100

    Remove container import from gallery.

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,5 +1,4 @@
 import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import getAlt from '../helpers/getAlt';
