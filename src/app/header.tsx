a456ced6748676d8e6015374e6426ef419ce20d7
Samuel Rodriguez <samuelyret@gmail.com>
2024-02-29 13:01:06 +0100

    Remove background color in navbar.

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="bg-body">
         <Navbar.Brand href="/">Casa Atlante</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
