c0b1fb0529208f422c606760b871acdb067e2e03
Samuel Rodriguez <samuelyret@gmail.com>
2024-03-24 16:00:21 +0100

    Update booking information with new email address.

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,7 +1,8 @@
 export default function Booking() {
   return (
     <p>
-      For booking enquiries please contact us at casa.atlante.lapalma@gmail.com
+      For booking enquiries please contact us at{' '}
+      <a href="mailto:booking@casa-atlante.com">booking@casa-atlante.com</a>
     </p>
   );
 }
