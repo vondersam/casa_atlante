31f96ba9d7a45249a0554bd1015edf447f94bbb6
Samuel Rodriguez <samuelyret@gmail.com>
2024-05-01 15:48:24 +0200

    Add bootstrap nav to links for better formatting (marking currently selected link).

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -8,17 +8,27 @@ function Header() {
   return (
     <>
       <Navbar expand="lg" className="bg-body mb-3">
-        <Link className="brand" href="/">
-          Casa Atlante
+        <Link href="/" passHref legacyBehavior>
+          <Navbar.Brand>Casa Atlante</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link href="/house">The house</Link>
-            <Link href="/location">Location</Link>
-            <Link href="/gallery">Gallery</Link>
-            <Link href="/booking">Booking</Link>
-            <Link href="/about">About</Link>
+            <Link href="/house" passHref legacyBehavior>
+              <Nav.Link>The house</Nav.Link>
+            </Link>
+            <Link href="/location" passHref legacyBehavior>
+              <Nav.Link>Location</Nav.Link>
+            </Link>
+            <Link href="/gallery" passHref legacyBehavior>
+              <Nav.Link>Gallery</Nav.Link>
+            </Link>
+            <Link href="/booking" passHref legacyBehavior>
+              <Nav.Link>Booking</Nav.Link>
+            </Link>
+            <Link href="/about" passHref legacyBehavior>
+              <Nav.Link>About</Nav.Link>
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
